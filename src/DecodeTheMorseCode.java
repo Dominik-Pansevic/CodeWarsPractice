f6263ff6d0b16ffb24a36d6ca922b40be5661b0b
Dominik-Pansevic <dominik.pansevic@gmail.com>
2021-03-14 17:17:11 +0000

    Add 'Multiples of 3 or 5' Kata

--- src/DecodeTheMorseCode.java
+++ src/DecodeTheMorseCode.java
@@ -1,15 +1,15 @@
 public class DecodeTheMorseCode {
-    public static String decode(String morseCode) {
-        String[] characters = morseCode.split("\\s");
-        String result = "";
-
-        for (int i = 0; i<characters.length; i++) {
-            if(characters[i].equals("")){
-                result += " ";
-            } else {
-                result += MorseCode.get(characters[i]);
-            }
-        }
-        return result.trim().replaceAll("\\s+", " ");
-    }
+//    public static String decode(String morseCode) {
+//        String[] characters = morseCode.split("\\s");
+//        String result = "";
+//
+//        for (int i = 0; i<characters.length; i++) {
+//            if(characters[i].equals("")){
+//                result += " ";
+//            } else {
+//                result += MorseCode.get(characters[i]);
+//            }
+//        }
+//        return result.trim().replaceAll("\\s+", " ");
+//    }
 }
